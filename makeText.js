4ef13ab84ca3156781580edc2020d98d44a81626
SeldomseenSchweig <jamesschweig@gmail.com>
2022-09-09 09:35:47 -0700

    finished

--- makeText.js
+++ makeText.js
@@ -1,2 +1,48 @@
-/** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const axios = require('axios').default;
+const markov = require('./markov')
+const process = require('process')
 
+
+
+
+if (process.argv[2] =='url')
+{
+    let filename = process.argv[3]
+    if (isValidHttpUrl(filename)){
+        webResponse(filename)
+         
+    }else{
+        console.log(` Url: ${process.argv[3]} is invalid`)
+        process.kill(1)
+    }
+}else if(process.argv[2] =='file'){
+    let filename =  process.argv[3]
+    fs.readFile(filename, "utf8", (err, data)=> {
+        if(err){
+            console.log(err)
+            process.kill(1)
+            
+        }
+        new markov.MarkovMachine(data)
+    })
+}
+
+async function webResponse(filename){
+    const response = await axios.get(filename);
+    data=response.data;
+    new markov.MarkovMachine(data)
+}
+
+
+function isValidHttpUrl(string) {
+    let url;
+    
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;  
+    }
+  
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
--- markov.js
+++ markov.js
@@ -17,17 +17,22 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
+    // set up the container for the markov machine words
     let chain = new Object
     let i = 0
     let length = (this.words).length
+    // for the last word in the map put null
     chain[this.words[length-1]] = []
     chain[this.words[length-1]].push(null);
+    // got through the map, if the word is already in the chain, add the next wrod to it's array
+   
 
           while (i <length-1) {
             if(this.words[i] in chain){
                 chain[this.words[i]].push(this.words[i+1]);
                 i++
               }else{
+                 // if it's not in the array, set up an empty array for it's value and add the next word to it's value
                 chain[this.words[i]] = [];
                 chain[this.words[i]].push(this.words[i+1]);
                 i++
@@ -35,39 +40,45 @@ class MarkovMachine {
             }
     this.makeText(chain)
     
-
     }
-
   
-
-
   /** return random text from chains */
 
   makeText(item, numWords = 100) {
-    console.log(item)
+    
     let keyLength = 0
     let word = ''
-
+    // Get all the keys from the object chain
     const keys = Object.keys(item);
-    let randKey = keys[Math.floor(Math.random() * keys.length)];
-    
+    //get a random key from the chain and add it as the first word in the array
+    let randKey = keys[Math.floor(Math.random() * keys.length-1)];
+    //getting a single value to add as a starter to the erray
+    let starter =  item[randKey][Math.floor(Math.random() * item[randKey].length)]
     let script = []
-    script.push(item[randKey])
+    script.push(starter)
 
     let i = 0;
 
         while(i < numWords){
           // Getting the legth of values for the current letter
+          try {
             keyLength = item[script[i]].length;
+          } catch (error) {
+            console.log(i)
+            console.log(script[i])
+            console.log(item[script[i]])
+            process.kill(1)
+ 
+          }
+
           // choosing a random value(next word) from the values of the current word 
             word = item[script[i]][ Math.floor(Math.random() * keyLength)];
 
-          // if the word chosen is null, kill the app, print the string, we'l do it live!
+          // if the word chosen is null, kill the app, print the string
             if(word===null){
             // join the string, add a period, call it a day
               script = script.join(" ")
               script += "."
-              console.log(script)
               return script
 
             }else{
@@ -88,4 +99,9 @@ class MarkovMachine {
   }
 }
 
-m = new MarkovMachine('cat in the hat lat blue pink red orange blue blue blue blue black red green purple he didnt know where to got so went on his blue')
+module.exports ={
+
+  MarkovMachine,
+
+
+};
