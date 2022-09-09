0d29f3f0b3cf8c8ece0cd6dd12367d03572b0831
SeldomseenSchweig <jamesschweig@gmail.com>
2022-09-08 19:31:29 -0700

    runs correctly 80% of the time, then gets a length error

--- markov.js
+++ markov.js
@@ -9,7 +9,6 @@ class MarkovMachine {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
     this.makeChains();
-    this.chain = {}
   }
 
   /** set markov chains:
@@ -20,20 +19,23 @@ class MarkovMachine {
   makeChains() {
     let chain = new Object
     let i = 0
+    let length = (this.words).length
+    chain[this.words[length-1]] = []
+    chain[this.words[length-1]].push(null);
 
-
-          while (i < (this.words).length - 1) {
-
-              if(this.words[i] in chain){
-                chain[this.words[i]].push(this.words[i+1])
+          while (i <length-1) {
+            if(this.words[i] in chain){
+                chain[this.words[i]].push(this.words[i+1]);
                 i++
               }else{
-                chain[this.words[i]] = []
-                chain[this.words[i]].push(this.words[i+1])
+                chain[this.words[i]] = [];
+                chain[this.words[i]].push(this.words[i+1]);
                 i++
               }
             }
-            console.log(chain)
+    this.makeText(chain)
+    
+
     }
 
   
@@ -41,11 +43,49 @@ class MarkovMachine {
 
   /** return random text from chains */
 
-  makeText(numWords = 100) {
+  makeText(item, numWords = 100) {
+    console.log(item)
+    let keyLength = 0
+    let word = ''
+
+    const keys = Object.keys(item);
+    let randKey = keys[Math.floor(Math.random() * keys.length)];
+    
+    let script = []
+    script.push(item[randKey])
+
+    let i = 0;
+
+        while(i < numWords){
+          // Getting the legth of values for the current letter
+            keyLength = item[script[i]].length;
+          // choosing a random value(next word) from the values of the current word 
+            word = item[script[i]][ Math.floor(Math.random() * keyLength)];
+
+          // if the word chosen is null, kill the app, print the string, we'l do it live!
+            if(word===null){
+            // join the string, add a period, call it a day
+              script = script.join(" ")
+              script += "."
+              console.log(script)
+              return script
+
+            }else{
+              //If it's not null, pop it into the array
+              script.push(word)
+              i++
+            }
+          }
+// if null is never hit, join the array, end program, print sentence
+    script = script.join(" ")
+    script += "."
+    console.log(script)
+    return script 
+
+
+
 
   }
 }
 
-m = new MarkovMachine('cat in the hat lat blue pink red orange blue blue blue blue black red green purple he didnt know where to got so went on his way')
-
-m.makeText()
+m = new MarkovMachine('cat in the hat lat blue pink red orange blue blue blue blue black red green purple he didnt know where to got so went on his blue')
